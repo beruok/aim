edba34a38c65dd34ff491195f65bc6be11afe43b
beruok <akafuku1112kazu77@outlook.jp>
2025-04-24 23:56:06 +0900

    fix: set host and port for Render

--- app.py
+++ app.py
@@ -96,4 +96,4 @@ def index():
     return render_template("index.html", result=result, result_sorted=result_sorted, error=error)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
